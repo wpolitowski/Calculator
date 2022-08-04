ef2ddb0f0c240835b42ccc80bc9554b5a1ed70c5
wpolitowski <wojciechpolitowski@protonmail.ch>
2022-08-04 16:30:10 +0200

    Enable simple calculations when entered in a desired order (number, operator, number)

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,9 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, b, operator) {
+function operate(a, operator, b) {     
+    a = parseFloat(a);
+    b = parseFloat(b);
     switch(operator) {
         case '+':
             return add(a, b);
@@ -26,12 +28,51 @@ function operate(a, b, operator) {
 
             return multiply(a, b);
             break;
-        case '/':
+        case '÷':
             return divide(a, b);
             break;
         default:
             alert("No such operation");
             break;
-    }
+    }    
 }
 
+let currOp = ['','','']; // array for storing current operation - two operands and one operator between
+
+const resultDiv = document.querySelector('.result');
+const buttons = document.querySelectorAll('button');
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
+
+const numbers = Array.from(buttons)
+    .filter(button => /[0-9]/.test(button.textContent));
+
+numbers.forEach(number => number.addEventListener('click', addOperand));
+operators.forEach(operator => operator.addEventListener('click', addOperator));
+
+
+equals.addEventListener('click', () => {
+    if (!currOp.includes('') && currOp.length === 3) {
+        const result = operate(...currOp);
+        resultDiv.textContent = result;
+        currOp = [result,'',''];
+    }
+});
+
+function addOperand() {
+    if (!currOp[1]) currOp[0] += this.textContent; //adds first operand
+    else currOp[2] += this.textContent;//adds second operand only when operator has been selected 
+    // else return;
+
+    resultDiv.textContent += this.textContent;
+}
+
+function addOperator() {
+    if (currOp[0] && !currOp[2]) {
+        if (!currOp[1]) resultDiv.textContent += this.textContent;
+        else resultDiv.textContent =
+            resultDiv.textContent.slice(0,-1) + this.textContent; //this allows to change operator for another one      
+        
+        currOp[1] = this.textContent;
+    }
+}
